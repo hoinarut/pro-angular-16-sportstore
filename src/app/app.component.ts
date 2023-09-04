93892e16855a89d898a89a156a223962518d7047
hoinarut-cv <tudor.hoinaru@code-ventures.ro>
2023-09-04 10:40:58 +0300

    Chapter 6.3.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-2 text-center text-white">
-                This is SportsStore
-             </div>`,
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {StoreModule} from "./store/store.module";
+import {RouterModule} from "@angular/router";
+import {StoreComponent} from "./store/store.component";
+import {CartDetailComponent} from "./store/cartDetail.component";
+import {CheckoutComponent} from "./store/checkout.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule,
+    RouterModule.forRoot([
+      {path: "store", component: StoreComponent},
+      {path: "cart", component: CartDetailComponent},
+      {path: "checkout", component: CheckoutComponent},
+      {path: "**", redirectTo: "/store"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
