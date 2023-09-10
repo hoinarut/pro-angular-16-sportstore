339f2c182f50f6e30d4a12c1ed41a75b32c6c439
hoinarut-cv <tudor.hoinaru@code-ventures.ro>
2023-09-10 10:31:15 +0300

    Chapter 7.15

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -6,6 +6,7 @@ import {Order} from "./order.model";
 import {OrderRepository} from "./order.repository";
 import {HttpClientModule} from "@angular/common/http";
 import {RestDataSource} from "./rest.datasource";
+import {AuthService} from "./auth.service";
 
 @NgModule({
   imports: [HttpClientModule],
@@ -15,7 +16,8 @@ import {RestDataSource} from "./rest.datasource";
     Cart,
     Order,
     OrderRepository,
-    RestDataSource
+    RestDataSource,
+    AuthService
   ]
 })
 export class ModelModule {
